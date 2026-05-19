27ac571cc75bcb9d2b1133bfdcc3f9f0d9dbc653
ethanlenox <noxleviox@gmail.com>
2026-05-19 10:27:59 +0200

    Update player.js

--- player.js
+++ player.js
@@ -737,7 +737,32 @@ const newGain = AudioCore.nextGain();
       newAudio.currentTime = 0;
 newAudio.muted = State.muted;
 
-      await newAudio.play();
+
+
+     //T E S T
+     
+      aconst p = newAudio.play();
+
+if (p !== undefined) {
+
+  await p.catch(err => {
+
+    console.error("PLAY ERROR:", err);
+
+    alert(
+      "PLAY ERROR: " +
+      err.name +
+      " / " +
+      err.message
+    );
+
+    throw err;
+
+  });
+
+}
+
+     //T E S T 
 
 // sécurité race condition async
 if(token !== this.playToken){
