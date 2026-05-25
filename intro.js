0f1c302817bb7709a9ec52218df32abb45557a1f
ethanlenox <noxleviox@gmail.com>
2026-05-25 05:28:28 +0200

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,1 +1,19 @@
+window.addEventListener("load", () => {
 
+  const intro = document.getElementById("intro");
+
+  // durée affichage intro
+  const INTRO_DURATION = 3200;
+
+  setTimeout(() => {
+
+    intro.classList.add("hide");
+
+    // nettoyage propre DOM
+    setTimeout(() => {
+      intro.remove();
+    }, 1400);
+
+  }, INTRO_DURATION);
+
+});
